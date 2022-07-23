b411ba41a8acc57b072390ab5d7e49b02f64fcd7
leovct <l009.vincent@gmail.com>
2022-07-23 09:55:47 +0200

    Enable wallets to connect to the dapp

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,70 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@rainbow-me/rainbowkit/styles.css"
+import {
+	RainbowKitProvider,
+	getDefaultWallets,
+	connectorsForWallets,
+	wallet,
+	lightTheme,
+} from "@rainbow-me/rainbowkit"
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi"
+import { alchemyProvider } from "wagmi/providers/alchemy"
+import { publicProvider } from "wagmi/providers/public"
+
+const { chains, provider, webSocketProvider } = configureChains(
+	[chain.polygonMumbai],
+	[
+		alchemyProvider({ alchemyId: process.env.ALCHEMY_API_KEY }),
+		publicProvider(),
+	]
+)
+
+const { wallets } = getDefaultWallets({
+	appName: "Avocado",
+	chains,
+})
+
+const connectors = connectorsForWallets([
+	...wallets,
+	{
+		groupName: "Other",
+		wallets: [
+			wallet.argent({ chains }),
+			wallet.trust({ chains }),
+			wallet.ledger({ chains }),
+		],
+	},
+])
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+	webSocketProvider,
+})
+
+const appInfo = {
+	appName: "Avocado",
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider
+				appInfo={appInfo}
+				chains={chains}
+				showRecentTransactions={true}
+				theme={lightTheme({
+					accentColor: "#84aa21",
+					overlayBlur: "small",
+				})}
+				coolMode
+			>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	)
 }
 
 export default MyApp
--- app/sections/shared/Header.tsx
+++ app/sections/shared/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react"
 import styled from "styled-components"
 import Image from "next/image"
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 import Avocado from "../../assets/avocado.png"
 
@@ -9,22 +10,31 @@ const IMG_SIZE = "60px"
 const Header: FC = () => {
 	return (
 		<Container>
-			<Image src={Avocado} alt="avocado" priority={true} width={IMG_SIZE} height={IMG_SIZE} layout="fixed"/>
-			<h1>avocado.io</h1>
+			<TitleContainer>
+				<Image src={Avocado} alt="avocado" priority={true} width={IMG_SIZE} height={IMG_SIZE} layout="fixed"/>
+				<h1>avocado</h1>
+			</TitleContainer>
+			<ConnectButton />
 		</Container>
 	)
 }
 
 const Container = styled.div`
 	/* Layout */
-	height: 200px;
 	display: flex;
-	justify-content: center;
+	justify-content: space-between;
 	align-items: center;
-	gap: 16px;
+	margin: 40px;
 
 	/* Color */
 	color: #84aa21;
+`
+
+const TitleContainer = styled.div`
+	/* Layout */
+	display: flex;
+	align-items: center;
+	gap: 16px;
 `
 
 export default Header
